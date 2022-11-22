1502998b22d22c53051b8d88d0e33b2b28e70e51
mendes1 <114608485+Mendes00@users.noreply.github.com>
2022-11-22 19:37:21 +0000

    GGG

--- main.c
+++ main.c
@@ -34,59 +34,54 @@ int main() {
     if (escolha_computador == 2) {
       printf("O computador escolheu Papel:\n\n");
     }
-    if (escolha_computador == 3) {
-      printf("O computador escolheu Tesoura:\n\n");
+    #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int pontos_jogador=0, pontos_cpu=0;
+main()
+{
+    jogar();
+}
+int jogar()
+{
+    int jogador, cpu;
+    printf ("\n-jogar-\n\n");
+    printf ("%d Jogador X CPU %d\n", pontos_jogador, pontos_cpu);
+    printf ("-------------\n");
+    printf ("0. Pedra\n");
+    printf ("1. Papel\n");
+    printf ("2. Tesoura\n");
+    printf ("3. Sair\n\nSua escolha:");
+    scanf ("%d", &jogador);
+    if (jogador < 0 || jogador >=3)
+    {
+        exit(0);
     }
-    if (escolha_computador == 4) {
-      printf("O computador escolheu Lagarto:\n\n");
+    srand(time(NULL));
+    cpu = rand() % 3; //gera um numero aleatorio
+    switch(cpu)
+    {
+        case 0: printf ("CPU -> Pedra\n"); break;
+        case 1: printf ("CPU -> Papel\n"); break;
+        case 2: printf ("CPU -> Tesoura\n"); break;
     }
-    if (escolha_computador == 5) {
-      printf("O computador escolheu Spock:\n\n");
-    }
-    if (escolha_jogador == escolha_computador) {
-      printf("Empate:\n");
-    }
-    if (escolha_jogador == 1 && escolha_computador == 3)
-      printf("você ganhou;\n");
-    if (escolha_jogador == 2 && escolha_computador == 1)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 3 && escolha_computador == 2)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 1 && escolha_computador == 4)
-      printf("você ganhou;\n");
-    if (escolha_jogador == 2 && escolha_computador == 5)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 3 && escolha_computador == 4)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 5 && escolha_computador == 3)
-      printf("você ganhou;\n");
-    if (escolha_jogador == 5 && escolha_computador == 1)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 4 && escolha_computador == 2)
-      printf("você ganhou:\n");
-    if (escolha_jogador == 4 && escolha_computador == 5)
-      printf("você ganhou:\n");
-    else {
-      if (escolha_jogador == 3 && escolha_computador == 1)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 1 && escolha_computador == 2)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 2 && escolha_computador == 3)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 3 && escolha_computador == 5)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 1 && escolha_computador == 5)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 5 && escolha_computador == 4)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 4 && escolha_computador == 1)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 5 && escolha_computador == 2)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 4 && escolha_computador == 3)
-        printf("voçê perdeu:\n");
-      if (escolha_jogador == 2 && escolha_computador == 4)
-        printf("voçê perdeu:\n");
-    }
-  } while (escolha_jogador != 0);
+    if ((jogador == 0 && cpu==2) || (jogador == 1 && cpu == 0) || (jogador == 2 && cpu == 1)) //verifica se o jogador venceu
+        {
+            printf("Voce venceu!\n");
+            pontos_jogador++;
+            jogar();
+        }
+    if (jogador == cpu) //verifica se houve empate
+        {
+            printf ("Empate!\n");
+            jogar();
+        }
+    else
+        {
+            printf ("CPU venceu!\n");
+            pontos_cpu++;
+            jogar();
+
+        }
+        return 0;
 }
